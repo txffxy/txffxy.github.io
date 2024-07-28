fb18ca5d4cd1e8f325e4adf1b6a0e5e1a72709c5
李天翔 <80887309@qq.com>
2024-07-28 17:05:55 +0800

    feat(dev): 输入表单

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,79 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <h1>Chiness Fortune Telling</h1>
+    <h2>Said boe Fortune-telling,Prediction of your life</h2>
+    <el-divider />
+    <el-form ref="formRef" :model="form" :rules="rules" label-width="auto" status-icon>
+      <el-form-item label="Date of Birth" prop="date1">
+        <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" />
+      </el-form-item>
+      <el-form-item label="Time" prop="date2">
+        <el-time-picker v-model="form.date2" placeholder="Pick a time" />
+      </el-form-item>
+      <el-form-item label="Gender" prop="gender">
+        <el-radio-group v-model="form.gender">
+          <el-radio value="Male">Male</el-radio>
+          <el-radio value="Female">Female</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="handleSubmit(formRef)">GET YOUT ANSWER</el-button>
+      </el-form-item>
+    </el-form>
   </main>
 </template>
 
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
+
+const formRef = ref()
+const form = reactive({
+  date1: '',
+  date2: '',
+  gender: ''
+})
+
+const rules = reactive({
+  date1: [
+    {
+      type: 'date',
+      required: true,
+      message: 'Please pick a date',
+      trigger: 'change'
+    }
+  ],
+  date2: [
+    {
+      type: 'date',
+      required: true,
+      message: 'Please pick a time',
+      trigger: 'change'
+    }
+  ],
+  gender: [
+    {
+      required: true,
+      message: 'Please select gender',
+      trigger: 'change'
+    }
+  ]
+})
+
+const handleSubmit = async (formEl) => {
+  console.log('handleSubmit', form)
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
+}
+</script>
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+main {
+  margin: 0 auto;
+  max-width: 800px;
+  text-align: center;
 }
 </style>
